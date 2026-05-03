57f81d2b43d35e28ee7e7697d10ec05de5dbfb58
Battena Sai Nandini <battena.nandini@g10x.com>
2026-05-03 21:58:26 +0530

    day6 functions tasks completed

---
+++ js/day6.js
@@ -0,0 +1,33 @@
+// Day 6 Tasks
+
+// 1. kmToMiles
+function kmToMiles(km) {
+  return km * 0.621;
+}
+
+console.log(kmToMiles(10)); // 6.21
+
+
+// 2. gstAmount
+function gstAmount(price, rate = 18) {
+  return (price * rate) / 100;
+}
+
+console.log(gstAmount(1000)); // 180
+
+
+// 3. fullName
+function fullName(first, last) {
+  return first + " " + last;
+}
+
+console.log(fullName("Sai", "Nandini")); // Sai Nandini
+
+
+// 4. isAdult
+function isAdult(age) {
+  return age >= 18;
+}
+
+console.log(isAdult(20)); // true
+console.log(isAdult(15)); // false
